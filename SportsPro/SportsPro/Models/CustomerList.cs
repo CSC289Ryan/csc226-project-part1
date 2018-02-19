f6bc7066b8ec3b536f94c7b3750071c370707bff
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-02-19 13:01:59 -0500

    Adds indexers into list of customers

--- SportsPro/SportsPro/Models/CustomerList.cs
+++ SportsPro/SportsPro/Models/CustomerList.cs
@@ -5,5 +5,30 @@ using System.Web;
 
 namespace SportsPro.Models {
     public class CustomerList {
+        private List<Customer> customers;
+
+        public CustomerList() {
+            customers = new List<Customer>();
+        }
+
+        public Customer this[int index] {
+            get {
+                return customers[index];
+            }
+            set {
+                customers[index] = value;
+            }
+        }
+
+        public Customer this[string name] {
+            get {
+                foreach (Customer c in customers) {
+                    if (c.Name == name) {
+                        return c;
+                    }
+                }
+                return null;
+            }
+        }
     }
 }
