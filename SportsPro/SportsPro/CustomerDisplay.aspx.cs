1bfcaf10c2c525d15a7cee9571d87b959e6c4dba
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-02-18 22:14:08 -0500

    Populates page with info for selected customer

--- SportsPro/SportsPro/CustomerDisplay.aspx.cs
+++ SportsPro/SportsPro/CustomerDisplay.aspx.cs
@@ -4,11 +4,42 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using SportsPro.Models;
 
 namespace SportsPro {
     public partial class CustomerDisplay : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
+            if (!IsPostBack) {
+                ddlCustomers.DataBind();
+            }
+            Customer c = GetSelectedCustomer();
+            DisplayCustomerInfo(c);
+        }
 
+        private Customer GetSelectedCustomer() {
+            DataView customersTable = (DataView)
+                SqlDataSource1.Select(DataSourceSelectArguments.Empty);
+            customersTable.RowFilter = $"CustomerID = '{ddlCustomers.SelectedValue}'";
+            DataRowView row = customersTable[0];
+
+            Customer c = new Customer();
+            c.ID = (int)row["CustomerID"];
+            c.Name = row["Name"].ToString();
+            c.Address = row["Address"].ToString();
+            c.City = row["City"].ToString();
+            c.State = row["State"].ToString();
+            c.PostalCode = row["ZipCode"].ToString();
+            c.PhoneNumber = row["Phone"].ToString();
+            c.Email = row["Email"].ToString();
+            return c;
+        }
+
+        private void DisplayCustomerInfo(Customer c) {
+            string address = $"{c.Address}<br />{c.City}, {c.State} {c.PostalCode}";
+            lblAddress.Text = address;
+            lblPhone.Text = c.PhoneNumber;
+            lblEmail.Text = c.Email;
         }
     }
 }
--- SportsPro/SportsPro/CustomerDisplay.aspx.designer.cs
+++ SportsPro/SportsPro/CustomerDisplay.aspx.designer.cs
@@ -31,13 +31,13 @@ namespace SportsPro {
         protected global::System.Web.UI.WebControls.Label Label1;
         
         /// <summary>
-        /// DropDownList1 control.
+        /// ddlCustomers control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+        protected global::System.Web.UI.WebControls.DropDownList ddlCustomers;
         
         /// <summary>
         /// SqlDataSource1 control.
