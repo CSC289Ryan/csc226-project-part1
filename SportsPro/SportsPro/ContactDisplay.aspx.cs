6d388a3fa658cd20402bfb3777de194bafc0ed86
Ryan Hollinger <rahollinger@my.waketech.edu>
2018-02-19 16:58:11 -0500

    Loads contact list into listbox when viewing contact display page

--- SportsPro/SportsPro/ContactDisplay.aspx.cs
+++ SportsPro/SportsPro/ContactDisplay.aspx.cs
@@ -4,11 +4,28 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using SportsPro.Models;
 
 namespace SportsPro {
     public partial class ContactDisplay : System.Web.UI.Page {
-        protected void Page_Load(object sender, EventArgs e) {
+        private CustomerList contacts;
 
+        protected void Page_Load(object sender, EventArgs e) {
+            contacts = CustomerList.GetCustomers();
+            DisplayContactsOnFirstLoad();
+        }
+
+        private void DisplayContactsOnFirstLoad() {
+            if (!IsPostBack) {
+                DisplayContacts();
+            }
+        }
+
+        private void DisplayContacts() {
+            lstContacts.Items.Clear();
+            for (int idx = 0; idx < contacts.Count; idx++) {
+                lstContacts.Items.Add(new ListItem(contacts[idx].ContactDisplay()));
+            }
         }
 
         protected void btnRemoveContact_Click(object sender, EventArgs e) {
